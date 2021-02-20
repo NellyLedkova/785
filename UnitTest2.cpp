51bcb1d8a8802b7fc64f75fffe487cb81267637e
NellyLedkova <79341968+NellyLedkova@users.noreply.github.com>
2021-02-20 20:39:28 +0300

    Add files via upload

---
+++ UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication2/ConsoleApplication2.cpp"
+#include "../UnitTest2/pch.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int q = 5;
+			int r = Mass("Today was run");
+			Assert::IsTrue(q == r);
+		}
+	};
+}
